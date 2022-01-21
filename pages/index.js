a1ffa64f44725b916d2ac3c805f5cbb3d4285432
Chris Skudlarczyk <chris.skud@gmail.com>
2022-01-20 21:55:36 -0700

    transmitting to second brain

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Skudlarczyk Music" />
         <p className="description">
           Skudlarczyk music
         </p>
